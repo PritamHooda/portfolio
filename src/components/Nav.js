f7eb3416ea88a4184cb87be97e96f2927c5ac298
pritam hooda <hoodap@sheridancollege.ca>
2025-03-16 16:48:14 -0400

    added a navbar component

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Navbar() {
+    <nav>
+        <li><a href="#about">About</a></li>
+        <li><a href="#exp">Experience</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </nav>
+}
