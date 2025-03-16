4c8e374afed3c06f443e14d56a034891f537bb9c
pritam hooda <hoodap@sheridancollege.ca>
2025-03-16 16:54:16 -0400

    updated navbar and added more components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,14 @@
-import react from 'react';
+import React from 'react';
 
 function Navbar() {
-    <nav>
-        <li><a href="#about">About</a></li>
-        <li><a href="#exp">Experience</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-    </nav>
+    return (
+        <nav>
+            <li><a href="#about">About</a></li>
+            <li><a href="#exp">Experience</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </nav>
+    );
 }
+export default Navbar;
