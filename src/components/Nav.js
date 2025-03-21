ce2d50f74d828fa51154cd24885ad6902f2cd690
pritam hooda <hoodap@sheridancollege.ca>
2025-03-20 21:59:33 -0400

    updated navbar component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
+// Array contains nav bar items
+const navItems = [
+    {name: 'About', href: '#' },
+    {name: 'Experience', href: '#' },
+    {name: 'Skills', href: '#' },
+    {name: 'Projects', href: '#' },
+    {name: 'Contacts', href: '#' },
+];
+
 function Navbar() {
     return (
-        <nav>
-            <li><a href="#about">About</a></li>
-            <li><a href="#exp">Experience</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+        <nav className="bg-black flex justify-between items-center h-24 max-w-7xl mx-auto px-4 text-white">
+            <p className='ml-4 text-2xl'>Pritam Hooda</p>
+            <ul className='hidden md:flex'>
+                {navItems.map( item => (
+                    <li className='p-4 hover:bg-[#00df9a] hover:text-black rounded-xl m-2 cursor-pointer duration-300 '>
+                        <a href={item.href}>
+                        {item.name}
+                        </a>
+                    </li>
+                )) }
+            </ul>
         </nav>
     );
 }
